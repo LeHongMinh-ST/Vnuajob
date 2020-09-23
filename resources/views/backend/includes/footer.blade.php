a1cb213825b08d51e92455432dbec2d0644666ca
minhlh <hongminhle290@gmail.com>
2020-09-23 11:46:02 +0700

    first commit

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,17 @@
+ <!-- BEGIN BASE JS -->
+    <script src="{{asset('backend')}}/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/popper.js/umd/popper.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/bootstrap/js/bootstrap.min.js"></script> <!-- END BASE JS -->
+    <!-- BEGIN PLUGINS JS -->
+    <script src="{{asset('backend')}}/assets/vendor/pace-progress/pace.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/stacked-menu/js/stacked-menu.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/flatpickr/flatpickr.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/easy-pie-chart/jquery.easypiechart.min.js"></script>
+    <script src="{{asset('backend')}}/assets/vendor/chart.js/Chart.min.js"></script> <!-- END PLUGINS JS -->
+    <!-- BEGIN THEME JS -->
+    <script src="{{asset('backend')}}/assets/javascript/theme.min.js"></script> <!-- END THEME JS -->
+    <!-- BEGIN PAGE LEVEL JS -->
+    <script src="{{asset('backend')}}/assets/javascript/pages/dashboard-demo.js"></script> <!-- END PAGE LEVEL JS -->
+    <!-- <script src="{{asset('backend')}}/assets/javascript/pages/summernote-demo.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
