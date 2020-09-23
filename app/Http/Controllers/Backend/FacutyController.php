511371f499f264b5a04cea7dd991d1dbbf81480c
minhlh <hongminhle290@gmail.com>
2020-09-23 17:40:01 +0700

    tạo model và controller

---
+++ app/Http/Controllers/Backend/FacutyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Facuty;
+use Illuminate\Http\Request;
+
+class FacutyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Facuty  $facuty
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Facuty $facuty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Facuty  $facuty
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Facuty $facuty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Facuty  $facuty
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Facuty $facuty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Facuty  $facuty
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Facuty $facuty)
+    {
+        //
+    }
+}
